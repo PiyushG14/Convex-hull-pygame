2056a2f4d4e4d8c9f7e603c0de6c8ee20cd8c41e
PiyushG14 <46423477+PiyushG14@users.noreply.github.com>
2020-12-05 00:53:47 +0530

    Update convexhull.py

--- convexhull.py
+++ convexhull.py
@@ -6,6 +6,11 @@ point_color = (249, 127, 81)
 hull_color = (27, 156, 252)
 line_color = (37, 204, 247)
 
+def direction(a, b, c):
+    #(b-a) x (c-a)
+    #The points are according to python
+    return (((b[0]-a[0])*(c[1]-a[1]) - (b[1]-a[1])*(c[0]-a[0])) < 0)
+
 
 
 def main():    
@@ -25,6 +30,14 @@ def main():
     pygame.display.update()
 
     
+    #find leftmost point, considering only x coord
+    leftmost_x = min([xcoord[0] for xcoord in coords])
+    leftmost_point = [t for t in coords if leftmost_x is t[0]][0]
+    pygame.draw.circle(win, hull_color, leftmost_point, 4)
+    pygame.display.update()
+    
+    
+    current_point = leftmost_point
     
     while True: 
         for event in pygame.event.get():
